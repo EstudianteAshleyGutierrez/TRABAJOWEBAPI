e864469a744eb9f59244976125b51279a22e3d85
EstudianteAshleyGutierrez <liyonicol232003@gmail.com>
2022-11-14 17:17:35 -0500

    Se agrego el registro del pedido

--- src/components/Pedido/Registropedido.jsx
+++ src/components/Pedido/Registropedido.jsx
@@ -1,7 +1,7 @@
-import React,{Fragment,useEffect} from 'react';
+import React,{Fragment,useEffect,useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import axios from 'axios';
-//import {Modal,ModalBody,ModalFooter,ModalHeader, NavItem} from 'reactstrap';
+import axios from 'axios';
+import {Modal,ModalBody,ModalFooter,ModalHeader} from 'reactstrap';
 import Cookies from 'universal-cookie';
 import { useNavigate } from 'react-router-dom';
 
@@ -24,10 +24,213 @@ const Registropedido = () => {
       navigate('/');
     }
   },[]);
+
+  const baseUrl="https://localhost:44315/api/Pedidoes";
+  const [data,setdata]=useState([]);
+
+  const[modalInsertar,setmodalInsertar]=useState(false);
+  const[modalEditar,setmodalEditar]=useState(false);
+  const[modalEliminar,setmodalEliminar]=useState(false);
+
+  const [gestorSeleccionado,setGestorSeleccionado]=useState({
+    idPedido:'',
+    fechaPedido:'',
+    fechaEntrega:'',
+    idProveedor:'',
+   
+})
+
+const handlechange = (e) =>{
+  const{name,value}=e.target;
+  setGestorSeleccionado({
+    ...gestorSeleccionado,
+    [name]:value
+  });
+  console.log(gestorSeleccionado);
+}
+
+   //Constantes para abrir y cerrar los modales
+   const abrircerrarModalInsertar=() =>{
+    setmodalInsertar(!modalInsertar);
+  }
+
+  const abrircerrarModalEditar=()=>{
+    setmodalEditar(!modalEditar);
+  }
+  const abrircerrarModalEliminar=()=>{
+    setmodalEliminar(!modalEliminar);
+  }
+
+  const seleccionarGestor=(ordenes,caso)=>{
+    setGestorSeleccionado(ordenes);
+    (caso==="Editar")?
+    abrircerrarModalEditar(): abrircerrarModalEliminar();
+  }
+
+  const petiGET=async()=>{
+    await axios.get(baseUrl).then(resp=>{
+        setdata(resp.data);
+       
+      }).catch(error=>{
+        console.log(error);
+      })
+}
+
+
+const petiPost=async()=>{
+  delete gestorSeleccionado.idPedido;
+  await axios.post(baseUrl,gestorSeleccionado).then(resp=>{
+      setdata(data.concat(resp.data));
+      abrircerrarModalInsertar();
+      
+    }).catch(error=>{
+      console.log(error);
+    })
+    alert('El nuevo Pedido ha sido registrado exitosamente')
+
+}
+
+const petiPUT=async()=>{
+  await axios.put(baseUrl+"/"+gestorSeleccionado.idPedido,gestorSeleccionado).then(resp=>{
+      var respuesta = resp.data; 
+      var dataAuxiliar=data;
+      dataAuxiliar.map(ordenes=>{
+          if(ordenes.idPedido===gestorSeleccionado.idPedido){
+            ordenes.fechaPedido=respuesta.fechaPedido; 
+            ordenes.fechaEntrega=respuesta.fechaEntrega; 
+            ordenes.idProveedor=respuesta.idProveedor; 
+            
+          }
+      });
+      abrircerrarModalEditar();
+      
+      alert('Los datos del pedido Nº: '+ gestorSeleccionado.idPedido+' fueron actualizados correctamente')
+      
+    }).catch(error=>{
+      console.log(error);
+    })
+}
+
+
+
+//Cargar el combobox de proveedor
+const[proveedores,setproveedores]=useState([]);
+  const EspecialidadesGET=async() =>{
+    await axios.get("https://localhost:44315/api/BDTambo")
+    .then(resp=>{
+      setproveedores(resp.data);
+    }).catch(error=>{console.log(error)})
+  }
+
+
+
+useEffect(()=>{
+    petiGET();
+    EspecialidadesGET();
+},[])
+
+
+var nrofilas=data.length
   return (
     <Fragment>
-        <h2 className='text-center'>REGISTRO DE PEDIDOS</h2>
+      
+        <h2 className='text-center'><b>REGISTRO DE PEDIDOS</b></h2>
+        <br/>
+        <div>
+              <h5 className='text-primary'>Cantidad de Pedidos: {nrofilas}</h5>
+      </div>
+       <br/>
+        <button className="btn btn-success " onClick={()=>abrircerrarModalInsertar()}>NUEVO PEDIDO</button>{"   "}
         <button className='btn btn-danger' onClick={()=>cerrarSesion()}>CERRAR SESION</button>{"   "}
+      <br/>
+       <br/>
+       <table className='table table-bordered table- table-striped'>
+        <thead>
+            <tr>
+            <th>Id Pedido</th>
+            <th>Fecha Pedido</th>
+            <th>Fecha Entrega</th>
+            <th>Id Proveedor</th>
+            <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody className='text-start'>
+            {data.map(ordenes=>(
+            <tr key={ordenes.idPedido}>
+              <td>{ordenes.idPedido}</td>
+              <td>{ordenes.fechaPedido}</td>
+              <td>{ordenes.fechaEntrega}</td>
+              <td>{ordenes.idProveedor}</td>
+           
+              <td>
+                  <button className='btn btn-warning' onClick={()=>seleccionarGestor(ordenes,"Editar")} >EDITAR</button>{"  "}
+              </td>
+            </tr>
+            ))}
+              
+          </tbody>
+        </table>
+            
+
+        <Modal isOpen={modalInsertar}>
+                <ModalHeader>Registrar Pedido</ModalHeader>
+                <ModalBody>
+                    <div className="form-group">
+                      <label>Fecha Pedido:</label>
+                      <input type="date" className="form-control" name="fechaPedido" onChange={handlechange}/><br/>
+                      <br/>
+                      <label>Fecha de Entrega:</label>
+                      <input type="date" className="form-control" name="fechaEntrega" onChange={handlechange}/>
+                      <br/>
+                      <label>Proveedor:</label>
+                      <select name="idProveedor" className="form-control" onChange={handlechange}>
+                          {proveedores.map(prove =>(
+                              <option key={prove.idProveedor} value={prove.idProveedor}>
+                                  {prove.nombreProveedor}
+                              </option>
+                          ))}
+                      </select>
+                      <br/>
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <button className="btn btn-info" onClick={()=>petiPost()}>Registrar Pedido</button>{" |"}
+                    <button className="btn btn-danger" onClick={()=>abrircerrarModalInsertar()}>Cancelar</button>
+                </ModalFooter>
+            </Modal>
+
+            <Modal isOpen={modalEditar}>
+                <ModalHeader>Editar Pedido</ModalHeader>
+                <ModalBody>
+                    <div className="form-group">
+                    <label>IdPedido:</label>
+                      <input type="text" className="form-control" name="idPedido" value={gestorSeleccionado && gestorSeleccionado.idPedido} readOnly/>
+                      <label>Fecha de Pedido:</label>
+                      <input type="text" className="form-control" name="fechaPedido" onChange={handlechange}  value={gestorSeleccionado && gestorSeleccionado.fechaPedido} /><br/>
+                      <label>Fecha de Entrega:</label>
+                      <input type="text" className="form-control" name="fechaEntrega" onChange={handlechange}  value={gestorSeleccionado && gestorSeleccionado.fechaEntrega}/><br/>
+                      <br/>
+                      <label>Proveedor:</label>
+                      <select name="idProveedor" className="form-control"
+                           defaultValue={gestorSeleccionado.idProveedor} onChange={handlechange}>
+                             {proveedores.map(proves =>(
+                            <option key={proves.idProveedor} value={proves.idProveedor}>
+                                   {proves.nombreProveedor}
+                             </option>
+                             ))}
+                        </select>
+                      <br/>
+                     
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <button className="btn btn-info" onClick={()=>petiPUT()}>Actualizar Dato del Pedido</button>{" |"}
+                    <button className="btn btn-danger" onClick={()=>abrircerrarModalEditar()}>Cancelar</button>
+                </ModalFooter>
+            </Modal>
+
+
+
 
     </Fragment>
   )
